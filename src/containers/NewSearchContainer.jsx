c00cf5dd1940601797e5fd159394712264163fae
Triana Cerda <trianaRcerda@gmail.com>
2021-11-03 14:30:17 -0700

    added NewsSearchContainer to App.jsx + rendering to page in localhost

---
+++ src/containers/NewSearchContainer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NewsSearchContainer extends Component {
+  render() {
+    return <h1>News Search Container!!!!!</h1>;
+  }
+}
+
+export default NewsSearchContainer;
