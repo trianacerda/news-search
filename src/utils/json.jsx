52d392eaf7464702f306fa28f2f40162ec3257e1
Triana Cerda <trianaRcerda@gmail.com>
2021-11-04 13:36:30 -0700

    testing CI

---
+++ src/utils/json.jsx
@@ -0,0 +1,287 @@
+export default data = {
+  status: 'ok',
+  totalResults: 12168,
+  articles: [
+    {
+      source: [Object],
+      author: 'noreply@blogger.com (Unknown)',
+      title: 'Bitcoin hits strongest level since May',
+      description:
+        '<ul><li>Bitcoin, in terms of market value, rose 4.6% to $53,859.6.</li><li>It passed $50,000 mark for first time in four weeks on Tuesday. </li><li>Bitcoin fell below $50,000 in early September.</li></ul>Bitcoin hit its highest level since mid-May on Wednesda…',
+      url: 'https://techncruncher.blogspot.com/2021/10/bitcoin-hits-strongest-level-since-may.html',
+      urlToImage:
+        'https://blogger.googleusercontent.com/img/a/AVvXsEhiFZua2TKlWOHoZ5qipQ8zBVpSMbXNdxOFJmwNkddvji4GqJRO4SonCCmwV_IBrxtHCb2UlJ850RuZPY75s54QZkedzrYthaQqoo4tuOD3zUbMhTGxJ2T-rFtSu8I1vRJY9RtH8dWw7_5XtGfoeUvJuAWZU-U9XLsHKTKffBSWD6G9su34rjkO0sYD-w=w1200-h630-p-k-no-nu',
+      publishedAt: '2021-10-06T17:17:00Z',
+      content:
+        '<ul><li>Bitcoin, in terms of market value, rose 4.6% to $53,859.6.</li><li>It passed $50,000 mark for first time in four weeks on Tuesday. </li><li>Bitcoin fell below $50,000 in early September.</li>… [+821 chars]',
+    },
+    {
+      source: [Object],
+      author: 'noreply@blogger.com (Unknown)',
+      title:
+        'JPMorgan CEO Says Bitcoin Has No Intrinsic Value After Claiming That Its Price Could Rise 10X',
+      description:
+        'JPMorgan CEO Jamie Dimon is still not a Bitcoin fan despite rapid price appreciation   from U.Today https://ift.tt/3iyi5nR https://ift.tt/eA...',
+      url: 'https://techncruncher.blogspot.com/2021/10/jpmorgan-ceo-says-bitcoin-has-no.html',
+      urlToImage: null,
+      publishedAt: '2021-10-04T19:49:00Z',
+      content:
+        'JPMorgan CEO Jamie Dimon is still not a Bitcoin fan despite rapid price appreciationfrom U.Today https://ift.tt/3iyi5nR https://ift.tt/eA8V8J',
+    },
+    {
+      source: [Object],
+      author: 'noreply@blogger.com (Unknown)',
+      title: 'BTC, DOGE, and SHIBA Price Analysis for October 6',
+      description:
+        'How high are the chances of Bitcoin sustaining its gains and pushing altcoins higher?   from U.Today https://ift.tt/3BkD5px https://ift.tt/e...',
+      url: 'https://techncruncher.blogspot.com/2021/10/btc-doge-and-shiba-price-analysis-for.html',
+      urlToImage: null,
+      publishedAt: '2021-10-06T19:49:00Z',
+      content:
+        'How high are the chances of Bitcoin sustaining its gains and pushing altcoins higher?from U.Today https://ift.tt/3BkD5px https://ift.tt/eA8V8J',
+    },
+    {
+      source: [Object],
+      author: 'noreply@blogger.com (Unknown)',
+      title: 'Who Bought $1.6B in Bitcoin Wednesday, and Why?',
+      description:
+        'last week the cryptocurrency market persistently asked the gnawing and annoying question, “Why?”\r\n' +
+        'Specifically, why did someone make a massive purchase of $1.6 billion worth of bitcoin on Wednesday in a couple of minutes?\r\n' +
+        'While many see this huge buy as a si…',
+      url: 'https://techncruncher.blogspot.com/2021/10/who-bought-16b-in-bitcoin-wednesday-and.html',
+      urlToImage:
+        'https://blogger.googleusercontent.com/img/a/AVvXsEhsl_C2ZYUVeK-jmFi6Uqp_jp_ZkAFVcEXJD30f7HZvv6DGdmfboiV_UzjK5tbXge19JF93vpfwuN3KmtOaP-sfElPl1iTri7dE13Qn-epBs9qE93eEWjlP3lgWTuCsg_TxslHIoO1Dt61iSIYwmSVCJi4L-ap81CxS0pHD6eYqqClD11QGr_dfpPzQ6w=w1200-h630-p-k-no-nu',
+      publishedAt: '2021-10-10T20:12:00Z',
+      content:
+        'Specifically, why did someone make a massive purchase of $1.6 billion worth of bitcoin on Wednesday in a couple of minutes?\r\n' +
+        'While many see this huge buy as a signal of bullishness, there may be more… [+8443 chars]',
+    },
+    {
+      source: [Object],
+      author: 'Molly Taft',
+      title: 'Bitcoin Miners Are Gobbling Up U.S. Energy',
+      description:
+        'There’s a big new presence slurping up power from the U.S. grid, and it’s growing: bitcoin miners. New research shows that the U.S. has overtaken China as the top global destination for bitcoin mining and energy use is skyrocketing as a result.Read more...',
+      url: 'https://gizmodo.com/bitcoin-miners-are-gobbling-up-u-s-energy-1847956270',
+      urlToImage:
+        'https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/2a0e7e1f6d07e6cb21b75677aa6df5f0.jpg',
+      publishedAt: '2021-10-28T16:45:00Z',
+      content:
+        'Theres a big new presence slurping up power from the U.S. grid, and its growing: bitcoin miners. New research shows that the U.S. has overtaken China as the top global destination for bitcoin mining … [+3088 chars]',
+    },
+    {
+      source: [Object],
+      author: 'Molly Taft',
+      title: 'Mining Bitcoin Using Nuclear Power May Be Fine, Actually',
+      description:
+        'Last week, the Wall Street Journal ran a piece on three recent nuclear-bitcoin deals that may signal a growing trend in the industry. The Journal piece reflects a small but growing sense of excitement expressed by some crypto fans—including Miami mayor and bi…',
+      url: 'https://gizmodo.com/mining-bitcoin-using-nuclear-power-may-be-fine-actuall-1847825102',
+      urlToImage:
+        'https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/3cd3ec5c1c48d6ad49a659ab76b82e02.jpg',
+      publishedAt: '2021-10-08T16:35:00Z',
+      content:
+        'Last week, the Wall Street Journal ran a piece on three recent nuclear-bitcoin deals that may signal a growing trend in the industry. The Journal piece reflects a small but growing sense of excitemen… [+9512 chars]',
+    },
+    {
+      source: [Object],
+      author: 'Tom McKay',
+      title:
+        'Roughly One-Third of Bitcoin Is Controlled by a Small Cabal of Whales, According to New Study',
+      description:
+        'For all the talk of democratizing finance, the vast majority of Bitcoin continues to be owned by a relative handful of investors.Read more...',
+      url: 'https://gizmodo.com/roughly-one-third-of-bitcoin-is-controlled-by-a-small-c-1847938047',
+      urlToImage:
+        'https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/c5ef49718a8e4104ec3c1c55507c8244.jpg',
+      publishedAt: '2021-10-26T18:10:00Z',
+      content:
+        'For all the talk of democratizing finance, the vast majority of Bitcoin continues to be owned by a relative handful of investors.\r\n' +
+        'As flagged by Bloomberg, newly released data by the National Bureau … [+4274 chars]',
+    },
+    {
+      source: [Object],
+      author: 'noreply@blogger.com (Unknown)',
+      title:
+        'SEC Chair Dodges Question About Regulatory Status of Bitcoin and Ether',
+      description:
+        'The SEC boss remains mum about the regulatory status of the two largest cryptocurrenciesfrom U.Today https://ift.tt/3Af9H2O https://ift.tt/eA8V8J',
+      url: 'https://techncruncher.blogspot.com/2021/10/sec-chair-dodges-question-about.html',
+      urlToImage: null,
+      publishedAt: '2021-10-05T20:49:00Z',
+      content:
+        'The SEC boss remains mum about the regulatory status of the two largest cryptocurrenciesfrom U.Today https://ift.tt/3Af9H2O https://ift.tt/eA8V8J',
+    },
+    {
+      source: [Object],
+      author: 'noreply@blogger.com (Unknown)',
+      title:
+        'IMF Head Says Central Bank Digital Currencies Are Reliable, Hard to Think of Bitcoin as Money',
+      description:
+        'Digital currencies backed by central banks are the most reliable form of digital money, according to IMF managing director Kristalina Georgieva. At the same time, to think of cryptocurrencies such as bitcoin as money is difficult, believes the chief executive…',
+      url: 'https://techncruncher.blogspot.com/2021/10/imf-head-says-central-bank-digital.html',
+      urlToImage:
+        'https://blogger.googleusercontent.com/img/a/AVvXsEiXlCRA_UbzC-SYoJvcZ0FDmKyU1gg4tYoqZujE3S0M97uSwzeU3lozMz8_kXZysCa7VM_uc5T0Af1iZKwy3b5UlKNMliJ7ZnxrpONB3Pysz4zipt5vf1EGZidz_i2un_omh2aJ7ZQVCfqNr6D-lE31HNCIKXM7cof3ph2imr3uaLkwm4z3fG3E-smk_w=w1200-h630-p-k-no-nu',
+      publishedAt: '2021-10-06T17:39:00Z',
+      content:
+        'Digital currencies backed by central banks are the most reliable form of digital money, according to IMF managing director Kristalina Georgieva. At the same time, to think of cryptocurrencies such as… [+2917 chars]',
+    },
+    {
+      source: [Object],
+      author: 'Whitney Kimball',
+      title:
+        'A Bitcoin Mining Operation Started a Secret Power Plant, It Did Not Go as Planned',
+      description:
+        'A There Will Be Blood-style drama unfolds in Canada, where a Bitcoin mining operation illegally tapped an unused natural gas well near a wealthy neighborhood in Alberta. The company Link Global might have gotten away with pilfering gas, if not for noise compl…',
+      url: 'https://gizmodo.com/a-bitcoin-mining-operation-started-a-secret-power-plant-1847811718',
+      urlToImage:
+        'https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/7c2308365fbde2aacd00611609b7f19d.jpg',
+      publishedAt: '2021-10-06T19:40:00Z',
+      content:
+        'A There Will Be Blood-style drama unfolds in Canada, where a Bitcoin mining operation illegally tapped an unused natural gas well near a wealthy neighborhood in Alberta. The company Link Global might… [+2770 chars]',
+    },
+    {
+      source: [Object],
+      author: null,
+      title:
+        'Bitcoin edges off all-time high but momentum for more gains this year seen intact - Reuters',
+      description:
+        'Bitcoin fell slightly in Asian hours on Thursday, a day after marking an all-time high on optimism around the launch of the first U.S. bitcoin futures ETF.',
+      url: 'https://www.reuters.com/business/finance/bitcoin-edges-off-all-time-high-momentum-more-gains-this-year-seen-intact-2021-10-21/',
+      urlToImage:
+        'https://www.reuters.com/resizer/oQVnjJsX498T8jA_zabMC414KBs=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/VB4NPI74TNKYZLB4SQJW74UYPQ.jpg',
+      publishedAt: '2021-10-21T03:27:00Z',
+      content:
+        'Representation of cryptocurrency Bitcoin is placed on PC motherboard in this illustration taken, June 29, 2021. REUTERS/Dado Ruvic/IllustrationHONG KONG, Oct 21 (Reuters) - Bitcoin fell slightly in A… [+1642 chars]',
+    },
+    {
+      source: [Object],
+      author: null,
+      title:
+        'Riding the crypto rollercoaster: Bitcoin nears record high - Reuters',
+      description:
+        'Bitcoin is on the cusp of an all-time high, its latest rally fuelled by the launch of the first U.S. bitcoin futures exchange-traded fund that investors think could make the cryptocurrency accessible for swathes of new investors. <a href="/technology/bitcoin-…',
+      url: 'https://www.reuters.com/technology/riding-crypto-rollercoaster-bitcoin-nears-record-high-2021-10-20/',
+      urlToImage:
+        'https://www.reuters.com/resizer/dTT3e1T05rswsPG-lqqubNb2TrA=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/ITNSLRU3KFLP7ABJFCV4MG6BBY.jpg',
+      publishedAt: '2021-10-20T09:47:00Z',
+      content:
+        'A representation of the virtual cryptocurrency Bitcoin is seen in this picture illustration taken October 19, 2021. REUTERS/Edgar SuLONDON, Oct 20 (Reuters) - Bitcoin is on the cusp of an all-time hi… [+3260 chars]',
+    },
+    {
+      source: [Object],
+      author: 'ilee@insider.com (Isabelle Lee)',
+      title:
+        'The SEC could approve as many as 4 bitcoin futures ETFs in October',
+      description:
+        'Unlike bitcoin ETFs, SEC Chair Gary Gensler has expressed more openness towards bitcoin futures ETF.',
+      url: 'https://markets.businessinsider.com/news/currencies/bitcoin-futures-etf-cryptocurrency-sec-exchange-traded-fund-gary-gensler-2021-10',
+      urlToImage:
+        'https://images2.markets.businessinsider.com/616039325ae4fe0018a70e50?format=jpeg',
+      publishedAt: '2021-10-08T13:19:51Z',
+      content:
+        'Securities and Exchange Commission headquarters.\r\n' +
+        'REUTERS/Andrew Kelly\r\n' +
+        'The US Securities and Exchange Commission could approve as many as four bitcoin futures exchange-traded funds in October, in wh… [+1732 chars]',
+    },
+    {
+      source: [Object],
+      author: null,
+      title: 'Bitcoin nears record high ahead of futures ETF listing - Reuters',
+      description:
+        'Bitcoin hit a six-month high and was within striking distance of a record on Tuesday as traders bet an anticipated listing of a futures-based U.S. exchange traded fund could herald investment flows into bitcoin and cryptocurrency assets.',
+      url: 'https://www.reuters.com/technology/bitcoin-nears-record-high-ahead-futures-etf-listing-2021-10-19/',
+      urlToImage:
+        'https://www.reuters.com/resizer/jm8iK4NjlOkVWrOxCph3jw36h4A=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/IXILJWRYJZMRTBHUYL3KXS4KSA.jpg',
+      publishedAt: '2021-10-19T07:06:00Z',
+      content:
+        'A representation of the virtual cryptocurrency Bitcoin is seen in this picture illustration taken October 18, 2021. REUTERS/Edgar SuSINGAPORE, Oct 19 (Reuters) - Bitcoin hit a six-month high and was … [+2332 chars]',
+    },
+    {
+      source: [Object],
+      author: null,
+      title: 'Bitcoin sits below all-time high after U.S. ETF debut - Reuters',
+      description:
+        'Bitcoin  hovered below record levels on Wednesday, the day after the first U.S. bitcoin futures-based exchange-traded fund (ETF) began trading, a development that market participants say is likely to drive investment into the digital asset.',
+      url: 'https://www.reuters.com/technology/bitcoin-sits-below-all-time-high-after-us-etf-debut-2021-10-20/',
+      urlToImage:
+        'https://www.reuters.com/resizer/6SrJPhlyYOAq39obh31ZrIvLXUY=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/DFOD6OBDWJNB3ELM3Y5C2UPAN4.jpg',
+      publishedAt: '2021-10-20T04:03:00Z',
+      content:
+        'A representation of the virtual cryptocurrency Bitcoin is seen in this picture illustration taken October 19, 2021. REUTERS/Edgar SuHONG KONG, Oct 20 (Reuters) - Bitcoin hovered below record levels o… [+1954 chars]',
+    },
+    {
+      source: [Object],
+      author: 'Reuters Editorial',
+      title: 'Bitcoin tops $67,000 in record high - Reuters.com',
+      description:
+        'Bitcoin climbed to a record high on Wednesday amid new interest as the first U.S. bitcoin futures-based exchange-traded funds come to market.  Conway G. Gittens has more.',
+      url: 'https://www.reuters.com/video/watch/idOVEZYY6Q3',
+      urlToImage:
+        'https://static.reuters.com/resources/r/?d=20211020&i=OVEZYY6Q3&r=OVEZYY6Q3&t=2',
+      publishedAt: '2021-10-20T21:23:09Z',
+      content:
+        'Posted \r\n' +
+        'Bitcoin climbed to a record high on Wednesday amid new interest as the first U.S. bitcoin futures-based exchange-traded funds come to market. Conway G. Gittens has more.',
+    },
+    {
+      source: [Object],
+      author: 'Reuters Staff',
+      title: 'Bitcoin nears record high ahead of futures ETF listing - Reuters',
+      description:
+        'Bitcoin hit a six-month high and was within striking distance of a record on Tuesday as traders bet an anticipated listing of a futures-based U.S. exchange traded fund could herald investment flows into bitcoin and cryptocurrency assets.',
+      url: 'https://www.reuters.com/article/fintech-crypto-bitcoin-idUSKBN2H90JX',
+      urlToImage:
+        'https://static.reuters.com/resources/r/?m=02&d=20211019&t=2&i=1578273178&r=LYNXMPEH9I09A&w=800',
+      publishedAt: '2021-10-19T07:02:00Z',
+      content:
+        'By Reuters Staff\r\n' +
+        'SINGAPORE (Reuters) - Bitcoin hit a six-month high and was within striking distance of a record on Tuesday as traders bet an anticipated listing of a futures-based U.S. exchange tra… [+2206 chars]',
+    },
+    {
+      source: [Object],
+      author: 'insider@insider.com (Carla Mozée)',
+      title: 'The first-ever bitcoin ETF will start trading on Tuesday',
+      description:
+        'The arrival of the bitcoin-futures ETF comes as bitcoin this year has moved above $1 trillion in market capitalization.',
+      url: 'https://markets.businessinsider.com/news/currencies/proshares-bitcoin-strategy-etf-bito-nyse-first-futures-fund-launch-2021-10',
+      urlToImage:
+        'https://images2.markets.businessinsider.com/614c8e67c2c9630018f5f51e?format=jpeg',
+      publishedAt: '2021-10-18T13:53:14Z',
+      content:
+        'STR/NurPhoto via Getty Images\r\n' +
+        'The first-ever bitcoin-linked exchange-traded fund will begin trading on Tuesday, the provider ProShares said in a statement. The launch will mark another milestone in … [+1567 chars]',
+    },
+    {
+      source: [Object],
+      author: 'insider@insider.com (Carla Mozée)',
+      title:
+        "SEC punts on approving a spot bitcoin ETF again, pushing decision on Valkyrie's fund to 2022",
+      description:
+        'While investors wait for word on a spot bitcoin ETF, the SEC has let bitcoin futures ETFs launch.',
+      url: 'https://markets.businessinsider.com/news/currencies/spot-bitcoin-etf-sec-decision-delay-valkyrie-fund-cryptocurrency-gensler-2021-11',
+      urlToImage:
+        'https://images2.markets.businessinsider.com/60fb0c8f0729770012b9a811?format=jpeg',
+      publishedAt: '2021-11-02T13:39:41Z',
+      content:
+        'Bitcoin artwork displayed at the Bitcoin 2021 convention.\r\n' +
+        'Marco Bello/Getty Images\r\n' +
+        "The Securities and Exchange Commission will not make a decision on alternative asset management firm Valkyrie's pr… [+1492 chars]",
+    },
+    {
+      source: [Object],
+      author: 'Gertrude Chavez-Dreyfuss',
+      title:
+        'ANALYSIS-Bitcoin futures highlight some pitfalls for new ETFs - Reuters',
+      description:
+        'ANALYSIS-Bitcoin futures highlight some pitfalls for new ETFs  Reuters',
+      url: 'https://www.reuters.com/article/fintech-crypto-bitcoin-etf-idUSL1N2RB1ZQ',
+      urlToImage:
+        'https://s1.reutersmedia.net/resources_v2/images/rcom-default.png?w=800',
+      publishedAt: '2021-10-19T11:02:00Z',
+      content:
+        '* Cryotocurrency gets boost from ETF bitcoin futures launch\r\n' +
+        '* ETF bitcoins may have higher costs Vs buying of actual cryptos\r\n' +
+        '* But benefits of owning ETFs lauded by many despite some risks\r\n' +
+        '* Tuesd… [+4583 chars]',
+    },
+  ],
+};
